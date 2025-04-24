38919be668f35735282dd200e5335e8fd36254e4
Roller <ilnar1900@gmail.com>
2025-04-24 14:36:35 +0300

    last commit

--- src/main/java/ru/roller/CollectionsTask2/Main.java
+++ src/main/java/ru/roller/CollectionsTask2/Main.java
@@ -1,7 +1,17 @@
 package ru.roller.CollectionsTask2;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static ru.roller.CollectionsTask2.Task2.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String [] testArray = {"KFC", "Teremok", "Subway", "KFC", "KFC", "Teremok", "Subway", "KFC", "Subway", "KFC",
+                               "McDonalds"};
+
+        Map<String, Integer> result = mapToMap(testArray);
+
+        System.out.println(result);
     }
 }
--- src/main/java/ru/roller/CollectionsTask2/Task2.java
+++ src/main/java/ru/roller/CollectionsTask2/Task2.java
@@ -1,5 +1,28 @@
 package ru.roller.CollectionsTask2;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Task2 {
-    
+
+    public static <T> Map<T, Integer> mapToMap(T[] array) {
+        Map<T, Integer> result = new HashMap<>();
+
+        for (T element : array) {
+            Integer count = elementCounter(array, element);
+            result.put(element, count);
+        }
+
+        return result;
+    }
+
+    public static <T> Integer elementCounter(T[] array, T element) {
+        int counter = 0;
+
+        for(int i = 0; i < array.length; i++) {
+                if (array[i].equals(element))
+                    counter++;
+        }
+        return counter;
+    }
 }
